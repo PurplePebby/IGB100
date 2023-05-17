985dc0bf0c7f9d3c72aab2e4b6bfccc6e8a25080
PurplePebby <130005820+PurplePebby@users.noreply.github.com>
2023-05-17 21:53:55 +1000

    Pirate Boat
    Pirate moves between points, much like the shark.
    Uses Movepath.cs

---
+++ Group2_Project/Assets/Scripts/Movepath.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movepath : MonoBehaviour
+{
+    public Transform[] points;
+    int current;
+    public float speed = 5f;
+
+    private Vector3 positionOffset;
+    private Vector3 currentPosition;
+    private float angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePath();
+    }
+
+    private void MovePath() {
+        //enemy pathing https://www.youtube.com/watch?v=BGe5HDsyhkY
+        ; if (transform.position != points[current].position) {
+            LookAtNextPoint(points[current], 1);
+            transform.position = Vector3.MoveTowards(transform.position, points[current].position, speed * Time.deltaTime);
+        }
+        else {
+            current = (current + 1) % points.Length;
+        }
+
+    }
+
+    private void LookAtNextPoint(Transform P, int a) {
+        if (a == 0) {
+            transform.LookAt(P);
+            transform.Rotate(0f, 180f, 0f, Space.Self);
+        }
+        else {
+            transform.LookAt(P);
+
+            //remove this if shark model is facing in the wrong direction
+            transform.Rotate(0f, 90f, 0f, Space.Self);
+        }
+
+    }
+}
---
+++ Group2_Project/Assets/Scripts/PirateMechanic.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PirateMechanic : MonoBehaviour {
+    public float pirateHealth;
+    public float Dmg;
+    public float pirateSpeed;
+    public float waterLevelPoint;
+    private void Update() {
+        //move
+    }
+    private void OnCollisionEnter(Collision collision) {
+        if (collision == null) return;
+        if (collision.gameObject.tag == "Pew") {
+            //check health
+            if (pirateHealth <= 0) {
+                //kill boat
+                return;
+            }
+            else {
+                pirateHealth = pirateHealth - Dmg;
+            }
+            //lose health
+        }
+    }
+}
